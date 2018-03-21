9ab5e8a7751ccc20209194cc999e76d028b07f2d
Suresh <smsureshb4u@gmail.com>
2018-03-22 01:27:17 +0530

    Interceptor called

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,40 +1,31 @@
-import { Injectable } from '@angular/core';
-import { Http, Headers, RequestOptions,Response} from '@angular/http';
-import {User} from "../model/model.user";
-import 'rxjs/add/operator/map';
-import {AppComponent} from "../app.component";
+import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+
 @Injectable()
-export class AuthService {
-  constructor(public http: Http) { }
+export class HttpClient {
 
-  public logIn(user: User){
+  constructor(private http: Http) {}
+  
+  createAuthorizationHeader(headers: Headers) {
+    var clientCred: string = btoa( "testjwtclientid"+ ':' + "XY7kmzoNzl100");
+    headers.append('Authorization', 'Basic ' +
+      btoa(clientCred));
+      headers.append('Content-Type','application/x-www-form-urlencoded;charset=utf-w8'); 
+  }
 
+  get(url) {
     let headers = new Headers();
-    headers.append('Accept', 'application/json')
-    // creating base64 encoded String from user name and password
-    var base64Credential: string = btoa( user.username+ ':' + user.password);
-    headers.append("Authorization", "Basic " + base64Credential);
-
-    let options = new RequestOptions();
-    options.headers=headers;
-
-    return this.http.get(AppComponent.API_URL+"/account/login" ,   options)
-      .map((response: Response) => {
-      // login successful if there's a jwt token in the response
-      let user = response.json().principal;// the returned user object is a principal object
-      if (user) {
-        // store user details  in local storage to keep user logged in between page refreshes
-        localStorage.setItem('currentUser', JSON.stringify(user));
-      }
+    this.createAuthorizationHeader(headers);
+    return this.http.get(url, {
+      headers: headers
     });
   }
 
-  logOut() {
-    // remove user from local storage to log user out
-    return this.http.post(AppComponent.API_URL+"logout",{})
-      .map((response: Response) => {
-        localStorage.removeItem('currentUser');
-      });
-
+  post(url, data) {
+    let headers = new Headers();
+    this.createAuthorizationHeader(headers);
+    return this.http.post(url, data, {
+      headers: headers
+    });
   }
 }
